1d34d989bf6956eb45e23e90dbaa7e067218d759
imayanknegi29 <imayanknegi29@gmail.com>
2024-06-18 22:13:39 +0530

    Project created

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Mayank Negi on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
